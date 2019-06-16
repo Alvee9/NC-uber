7408e86d61dadc819b72dc5d8bd16fe296a7eb66
Alvee9 <abdullah.alvee@gmail.com>
2019-06-16 19:26:21 +0600

    Changes made before the end of the NewsCred Code Test

--- uber.cpp
+++ uber.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 
+int numberOfLocations = 100;
+int numberOfDrivers = 30;
+
 struct Location{
     string name;
     vector<pair<string, double> > dists;
@@ -87,7 +90,7 @@ void genGraph(int n){
 }
 
 
-
+set<int> dpos[500];
 
 void genDrivers(int n){
     srand(9999);
@@ -125,9 +128,15 @@ void genDrivers(int n){
         for(int j = 0; j < 6; j++){
             s += rand()%10 + '0';
         }
-
         drivers[i].carNumber = s;
 
+        drivers[i].curLocation = rand()%numberOfLocations + 1;
+        dpos[drivers[i].curLocation].insert(i);
+
+        drivers[i].fineCount = rand()%5;
+        drivers[i].fineSum = drivers[i].fineCount * 100;
+
+        drivers[i].rating = 3.0 + (rand()%10)/2.0;
 
 
     }
@@ -137,13 +146,118 @@ void genDrivers(int n){
 }
 
 
+int marked[300];
 
+struct Node1{
+    int itself;
+    double cost;
+    Node1(){}
+    Node1(int a, int b){
+        itself = a;
+        cost = b;
+    }
+    bool operator<(const Node1& a)const{
+        return cost<a.cost;
+    }
+};
+
+
+void getDist(int source){
+    memset(marked, 0, sizeof(marked));
+    priority_queue<Node1> pq;
+
+    pq.push(Node1(source, 0));
+
+    while(!pq.empty()){
+        Node1 tmp1 = pq.top();
+        pq.pop();
+
+        if (marked[tmp1.itself])
+            continue;
+
+        locations[source].dists.push_back(make_pair(locations[tmp1.itself].name, tmp1.cost));
+
+        marked[tmp1.itself] = 1;
+
+
+        for(int i = 0; i < G[tmp1.itself].size(); i++){
+            int v = G[tmp1.itself][i].first;
+            int w = G[tmp1.itself][i].second;
+            if (marked[v])
+                continue;
+            pq.push(Node1(v, tmp1.cost + w));
+        }
+
+
+    }
+
+    return;
+
+
+}
+
+
+
+
+
+
+pair<int, int> findDriver(int source, int passCount, double rating){
+    memset(marked, 0, sizeof(marked));
+    priority_queue<Node1> pq;
+
+    pq.push(Node1(source, 0));
+
+    while(!pq.empty()){
+        Node1 tmp1 = pq.top();
+        pq.pop();
+
+        if (marked[tmp1.itself])
+            continue;
+
+        if (dpos[tmp1.itself].size() > 0){
+            return make_
+        }
+
+
+        marked[tmp1.itself] = 1;
+
+        for(int i = 0; i < G[tmp1.itself].size(); i++){
+            int v = G[tmp1.itself][i].first;
+            int w = G[tmp1.itself][i].second;
+            if (marked[v])
+                continue;
+            pq.push(Node1(v, tmp1.cost + w));
+        }
+
+
+    }
+
+    return make_pair(-1, -1);
+
+
+}
 
 
 int main(){
-    genGraph(100);
+
+    genGraph(numberOfLocations);
+    genDrivers(numberOfDrivers);
+
+    for(int i = 1; i <= numberOfLocations; i++){
+        getDist(i);
+    }
+
+    while(true){
+        cout<<" enter source and destination locations, number of passengers, driver rating"<<endl;
+        string source, dest;
+        int passCount;
+        double rating;
+        cin>>source>>dest>>passCount>>rating;
 
 
+
+
+    }
 
 
 
